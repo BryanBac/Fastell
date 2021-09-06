11df1ebcffc0c55bc448bd903476367eb20f2db6
BryanBac <49624724+BryanBac@users.noreply.github.com>
2021-09-06 17:32:23 -0600

    Base Para el proyecto
    Utilizando un antiguo proyecto de base de datos se crea el repositorio a utilizar.

---
+++ src/proyecto/pkg/CuentaR.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.pkg;
+
+/**
+ *
+ * @author libreria6
+ */
+public class CuentaR {
+     String Nombre;
+    String Banco;
+    String NoCuenta;
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public void setBanco(String Banco) {
+        this.Banco = Banco;
+    }
+
+    public void setNoCuenta(String NoCuenta) {
+        this.NoCuenta = NoCuenta;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public String getBanco() {
+        return Banco;
+    }
+
+    public String getNoCuenta() {
+        return NoCuenta;
+    }
+    
+}
